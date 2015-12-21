1e0ee31b25d3988d7a938dfc71155f972469da6e
Jeff Minard <jeff@jrm.cc>
2015-12-21 14:10:57 -0800

    Add support for rotated videos not outputting correct size; reduce verbosity a lot; force overwrites (container is run non-interactively)

--- encode.sh
+++ encode.sh
@@ -14,33 +14,59 @@ IN=$1
 OUT=$(echo $1 | sed 's/^\(.*\)\.[a-zA-Z0-9]*$/\1/')
 
 echo "--- Encoding: $1"
+
+# We need to detect whether the video is rotated or not in order to
+# set the "scale" factor correctly, otherwise we can hit a fatal error
+# However, ffmpeg will automatically apply the rotation for us, so we
+# just need to ensure the scale is right, not also apply rotation.
+ROTATION=$(ffprobe $IN 2>&1 | \grep rotate | awk '{print $3}')
+if [ "$ROTATION" == "" ]; then
+    # No rotation, use normal scale (height 720, width auto)
+    SCALE="-1:720"
+    echo "--- No rotation detected"
+else
+    # Rotated video; we need to specify the scale the other way around
+    # to avoid a fatal "width not divisible by 2 (405x720)" error
+    # Instead we'll use (height auto, width 720)
+    SCALE="720:-1"
+    echo "--- Rotation detected; changed scale param"
+fi
+
 echo "--- webm, First Pass"
 ffmpeg -i $IN \
-    -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k \
-    -threads 0 -vf scale=-1:720 \
+    -hide_banner -loglevel error -stats \
+    -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 \
+    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
+    -threads 0 \
     -an -pass 1 -f webm -y \
     /dev/null
 
 echo "--- webm, Second Pass"
 ffmpeg -i $IN \
-    -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k \
-    -threads 0 -vf scale=-1:720 \
+    -hide_banner -loglevel error -stats \
+    -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 \
+    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
+    -threads 0 \
     -codec:a libvorbis -b:a 128k \
-    -pass 2 -f webm \
+    -pass 2 -f webm -y \
     $OUT.webm
 
 echo "--- x264, First Pass"
 ffmpeg -i $IN \
-    -codec:v libx264 -profile:v main -preset slow -b:v 1000k -maxrate 1000k -bufsize 2000k -vf scale=-1:720 \
+    -hide_banner -loglevel error -stats \
+    -codec:v libx264 -profile:v main -preset slow -b:v 1000k \
+    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
     -threads 0 -pass 1 \
     -an -f mp4 -y \
     /dev/null
 
 echo "--- x264, Second Pass"
 ffmpeg -i $IN \
-    -codec:v libx264 -profile:v main -preset slow -b:v 1000k -maxrate 1000k -bufsize 2000k -vf scale=-1:720 \
+    -hide_banner -loglevel error -stats \
+    -codec:v libx264 -profile:v main -preset slow -b:v 1000k \
+    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
     -threads 0 -pass 2 \
-    -codec:a libfdk_aac -b:a 128k -f mp4 \
+    -codec:a libfdk_aac -b:a 128k -f mp4 -y \
     $OUT.mp4
 EOF
 
