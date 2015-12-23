6e5e322be94a4506df554dc2ba02c30abe39ba15
Jeff Minard <jeff@jrm.cc>
2015-12-22 19:42:30 -0800

    Add core detection to speed up webm; format config params to be a bit more sensible; put "threads" command in correct spot to make it actually work

--- encode.sh
+++ encode.sh
@@ -32,42 +32,51 @@ else
     echo "--- Rotation detected; changed scale param"
 fi
 
+# Count cores, more than one? Use many!
+# Uses one less than total (recomendation for webm)
+# Doesn't apply to x264 where 0 == auto (webm doesn't support that)
+CORES=$(grep -c ^processor /proc/cpuinfo)
+if [ "$CORES" -gt "1" ]; then
+  CORES="$(($CORES - 1))"
+fi
+
+echo "--- Using $CORES threads for webm"
+
 echo "--- webm, First Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
-    -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 \
-    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
-    -threads 0 \
-    -an -pass 1 -f webm -y \
-    /dev/null
+    -codec:v libvpx -threads $CORES -slices 4 -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
+    -an \
+    -pass 1 \
+    -f webm \
+    -y /dev/null
 
 echo "--- webm, Second Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
-    -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 \
-    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
-    -threads 0 \
+    -codec:v libvpx -threads $CORES -slices 4 -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
     -codec:a libvorbis -b:a 128k \
-    -pass 2 -f webm -y \
-    $OUT.webm
+    -pass 2 \
+    -f webm \
+    -y $OUT.webm
 
 echo "--- x264, First Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
-    -codec:v libx264 -profile:v main -preset slow -b:v 1000k \
-    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
-    -threads 0 -pass 1 \
-    -an -f mp4 -y \
-    /dev/null
+    -codec:v libx264 -threads 0 -profile:v main -preset slow -b:v 1000k -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
+    -an \
+    -pass 1 \
+    -f mp4 \
+    -y /dev/null
 
 echo "--- x264, Second Pass"
 ffmpeg -i $IN \
     -hide_banner -loglevel error -stats \
-    -codec:v libx264 -profile:v main -preset slow -b:v 1000k \
-    -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
-    -threads 0 -pass 2 \
-    -codec:a libfdk_aac -b:a 128k -f mp4 -y \
-    $OUT.mp4
+    -codec:v libx264 -threads 0 -profile:v main -preset slow -b:v 1000k -maxrate 1000k -bufsize 2000k -vf scale=$SCALE \
+    -codec:a libfdk_aac -b:a 128k \
+    -pass 2 \
+    -f mp4 \
+    -y $OUT.mp4
 EOF
 
 # Run the container. Note that we set the workingdir to /tmp since there are a few cruft files
